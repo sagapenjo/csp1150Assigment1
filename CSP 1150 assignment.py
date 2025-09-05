2d22b808eb8fbbc34c4cba17a5e7f1f3b831f52c
Sagar Adhikari <sadhik15@our.ecu.edu.au>
2025-09-05 20:08:37 +0800

    i cannnot find the error

--- CSP 1150 assignment.py
+++ CSP 1150 assignment.py
@@ -1,10 +1,41 @@
 import time as t
 import random
 
-def create_q(maxnum, numofquestion):
+#make question
+def create_q(max_num):
     operations= ("+","-","*","/")
-    a=random.randint(1,maxnum)
-    b=random.randint(1,maxnum)
+    a=random.randint(1,max_num)
+    b=random.randint(1,max_num)
     op=random.choice(operations)
-     
-    
+    #store the question in a variable
+    result =eval((f"{a}{op}{b}"))
+    return result
+
+def ask_q(numofquestion):
+    print("Current score: 0")
+    score=0 
+    i=0
+    while i<numofquestion:
+        try:
+            answer= eval(input(result))
+            if answer == result:
+                i=+ 1
+                score=+1
+                print(f"Correct answer: {score}")
+            else:
+                print("Incorrect! ")
+        except ValueError:
+            print("enter a number")
+
+choice = input("Choose your difficulty: 1)Easy 2)Medium 3)Hard").lower()
+if choice == "1":
+    create_q(10)
+    ask_q(5)
+
+if choice == "2":
+    create_q(20)
+    ask_q(10)
+
+if choice == "3":
+    create_q(50)
+    ask_q(15)
