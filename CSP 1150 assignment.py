a9b88b9f5a3a7c7895a056e7e6120fbc4dfcb2fe
Sagar Adhikari <sadhik15@our.ecu.edu.au>
2025-09-06 18:00:16 +0800

    time error again

--- CSP 1150 assignment.py
+++ CSP 1150 assignment.py
@@ -14,24 +14,42 @@ def create_q(max_num,numofquestion):
 
 def ask_q(max_num,numofquestion):
     print("Current score: 0")
-    question=create_q(max_num, numofquestion)
     score=0 
-    i=0
+    i=0#new variable to control the number of displayed questions
     while i<numofquestion:
         try:
-            correct_ans = eval(question)
+            start_time = t.time()#start countiung time
+            question=create_q(max_num, numofquestion)
+            correct_ans = eval(question) #eval evaluates the operation intially counted as string
             user_ans = int(input(question))
+            
             if user_ans == correct_ans:
-                i= i+ 1
-                score= score + 1
+                i+= 1
+                score+= 1
                 print(f"Correct answer: {score}")
-            else:
-                print("Incorrect! ")
-                break
-        except ValueError:
-            print("enter a number")
+                end_time= t.time()#end counting time
+                elapsed_time = int(end_time - start_time)#calculate elapsed time
+                print(f"You took {round(elapsed_time,2)} seconds")
 
-choice = input("Choose your difficulty: 1)Easy 2)Medium 3)Hard").lower()
+
+            
+            else:
+                i+= 1              
+                print(f"Incorrect! Current score {score} ")                
+        
+        except ValueError:
+            print("Enter a number!!")
+    
+    average_score = (score/numofquestion)
+    print(f"""Quiz over! Your score was:  {score}/{numofquestion}
+You averaged {average_score}""")#only print after i=numofquestions(Multiline strings are very cool)
+
+choice = input("""Choose your difficulty: 
+1)Easy 
+2)Medium 
+3)Hard 
+-->""").lower()
+
 if choice == "1":
     ask_q(10,5)
 
