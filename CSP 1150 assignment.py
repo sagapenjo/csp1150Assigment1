73d0729ec20caf220a53618bf9041534c0ce32b2
Sagar Adhikari <sadhik15@our.ecu.edu.au>
2025-09-06 15:53:21 +0800

    added time system

--- CSP 1150 assignment.py
+++ CSP 1150 assignment.py
@@ -1,41 +1,42 @@
-import time as t
+import time as t #importing as t just makes it easier as i can wrtite t.start rather than time.start
 import random
 
 #make question
-def create_q(max_num):
-    operations= ("+","-","*","/")
-    a=random.randint(1,max_num)
-    b=random.randint(1,max_num)
-    op=random.choice(operations)
+def create_q(max_num,numofquestion):
+    while True:
+        operations= ("+","-")#write as string and use eval() later
+        a=random.randint(1,max_num)
+        b=random.randint(1,max_num)
+        op=random.choice(operations)
     #store the question in a variable
-    result =eval((f"{a}{op}{b}"))
-    return result
+        result =(f"{a}{op}{b} ")
+        return result #no brackets as im not calling a function
 
-def ask_q(numofquestion):
+def ask_q(max_num,numofquestion):
     print("Current score: 0")
+    question=create_q(max_num, numofquestion)
     score=0 
     i=0
     while i<numofquestion:
         try:
-            answer= eval(input(result))
-            if answer == result:
-                i=+ 1
-                score=+1
+            correct_ans = eval(question)
+            user_ans = int(input(question))
+            if user_ans == correct_ans:
+                i= i+ 1
+                score= score + 1
                 print(f"Correct answer: {score}")
             else:
                 print("Incorrect! ")
+                break
         except ValueError:
             print("enter a number")
 
 choice = input("Choose your difficulty: 1)Easy 2)Medium 3)Hard").lower()
 if choice == "1":
-    create_q(10)
-    ask_q(5)
+    ask_q(10,5)
 
-if choice == "2":
-    create_q(20)
-    ask_q(10)
+if choice == "2":    
+    ask_q(20,10)
 
 if choice == "3":
-    create_q(50)
-    ask_q(15)
+    ask_q(50,15)
