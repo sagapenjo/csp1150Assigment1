177b0bf8270885a13cd9f4f2f3d7b89c3cb58e26
Sagar Adhikari <sadhik15@our.ecu.edu.au>
2025-09-07 01:43:05 +0800

    final message

--- CSP 1150 assignment.py
+++ CSP 1150 assignment.py
@@ -1,6 +1,28 @@
 import time as t #importing as t just makes it easier as i can wrtite t.start rather than time.start
 import random
 
+print("--------------------------------------Welcome to the (not so) ULTIMATE math quiz--------------------------------------")
+choice = input("""Choose your difficulty: 
+1)Easy 
+2)Medium 
+3)Hard 
+-->""").lower()
+
+#Set difficulty parameters for chosen difficulty
+while True:
+    try:
+        if choice == "1":
+            ask_q(10,5)
+            break
+        elif choice == "2":    
+            ask_q(20,10)
+            break
+        elif choice == "3":
+            ask_q(50,15)
+            break
+    except ValueError:
+        ("Choose a Valid difficulty ")
+
 #make question
 def create_q(max_num,numofquestion):
     while True:
@@ -25,36 +47,23 @@ def ask_q(max_num,numofquestion):
             
             if user_ans == correct_ans:
                 i+= 1
-                score+= 1
-                print(f"Correct answer: {score}")
+                score+= 10
                 end_time= t.time()#end counting time
                 elapsed_time = int(end_time - start_time)#calculate elapsed time
-                print(f"You took {round(elapsed_time,2)} seconds")
-
-
-            
+                actual_score= (score-elapsed_time)
+                print(f"Correct answer! You took {round(elapsed_time,2)} seconds, Current score: {actual_score}")
+                 
             else:
                 i+= 1              
-                print(f"Incorrect! Current score {score} ")                
+                print(f"Incorrect! No points awarded ")                
         
         except ValueError:
-            print("Enter a number!!")
-    
-    average_score = (score/numofquestion)
-    print(f"""Quiz over! Your score was:  {score}/{numofquestion}
-You averaged {average_score}""")#only print after i=numofquestions(Multiline strings are very cool)
+            i+=1
+            print("Enter a number!! No points awarded")
+    average_time = (elapsed_time/numofquestion)
+    print(f"""Quiz over! 
+Your total score was:  {score}
+You average response time was: {average_time}s""")#only print after i=numofquestions(Multiline strings are very cool)
 
-choice = input("""Choose your difficulty: 
-1)Easy 
-2)Medium 
-3)Hard 
--->""").lower()
 
-if choice == "1":
-    ask_q(10,5)
-
-if choice == "2":    
-    ask_q(20,10)
-
-if choice == "3":
-    ask_q(50,15)
+        
